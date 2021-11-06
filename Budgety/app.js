258d15991b33f8271d6b8b96c6f984d5ffee3523
Enrique Ferrer <68733895+equeferrer@users.noreply.github.com>
2021-11-06 14:20:51 +0800

    draft tennis expenses calc

--- Budgety/app.js
+++ Budgety/app.js
@@ -1,17 +1,17 @@
 const budget = {
-    month: document.querySelector('.budget__title--month'),
     netBudgetBox: document.querySelector('.budget__value'),
     totalIncomeBox: document.querySelector('.budget__income--value'),
     totalExpensesBox: document.querySelector('.budget__expenses--value'),
     totalExpensesPercentageBox: document.querySelector('.budget__expenses--percentage'),
+    totalNumberOfPeopleBox: document.querySelector('.budget__expenses--number-of-people'),
+    totalExpensePerPersonBox: document.querySelector('.budget__expenses--value-per-people'),
     netBudget: 0,
     totalIncome: 0,
     totalExpenses: 0,
     totalExpensesPercentage: 0,
+    totalPeople: 4,
+    netPerPerson: 0,
 }
-
-let currentDate = new Date();
-budget.month.innerText = `${currentDate.toLocaleString('default', { month: 'long' })} ${currentDate.getFullYear()}`;
 
 // Helper function
 const formatter = new Intl.NumberFormat('fil-PH', {
@@ -24,12 +24,28 @@ budget.netBudgetBox.innerText = formatter.format(0);
 budget.totalIncomeBox.innerText = formatter.format(0);
 budget.totalExpensesBox.innerText = formatter.format(0);
 budget.totalExpensesPercentageBox.innerText = "...";
+budget.totalExpensePerPersonBox.innerText = formatter.format(0);
+budget.totalNumberOfPeopleBox.innerText = `${document.querySelector('.add__number__of__people').value} pax`;
 
 const inputDescription = document.querySelector(".add__description");
 const inputValue = document.querySelector(".add__value");
 const addBtn = document.querySelector(".add__btn");
+const inputPeople = document.querySelector(".add__number__of__people");
 const type = document.querySelector(".add__type");
 const container = document.querySelector('.container');
+
+let startingItems = document.querySelectorAll('.item__value');
+for (i = 0; i < startingItems.length; i++) {
+    let element = startingItems[i];
+
+    budget.totalExpenses += parseFloat(element.innerText)
+    budget.totalExpensesBox.innerText = formatter.format(budget.totalExpenses);
+}
+
+computePercentage();
+computeTotals();
+updatePercent();
+updatePerPerson();
 
 function changeColor(){
     if (type.value === "inc"){
@@ -52,24 +68,90 @@ container.addEventListener('click', (elem)=> {
     computePercentage();
     computeTotals();
     updatePercent();
+    updatePerPerson();
 });
 addBtn.addEventListener('click', () => {
-    if (inputValue.value > 0 && inputDescription.value !== ""){
+    if (inputValue.value > 0){
         addList();
-    } else if (inputDescription.value === "" || inputValue.value === ""){
-        alert("Must fill up all the fields");
+    } else if (inputValue.value === ""){
+        alert("Must fill up value field");
         return
     } else if (inputValue.value <= 0){
         alert("Cannot put a zero/negative value");
         return
     }
 })
+inputPeople.addEventListener("change", updatePerPerson)
+
+document.querySelectorAll('.item__quantity').forEach(item => {
+    item.addEventListener('change', () => {
+        originalPrice = item.getAttribute('id');
+        multiplier = item.value
+        if (isNaN(multiplier) || (multiplier < 0)){
+            return item.previousElementSibling.firstElementChild.innerText = formatter.format(0);
+        } else {
+            oldValue = item.previousElementSibling.previousElementSibling.firstElementChild.innerText;
+            newValue = multiplier * originalPrice
+        
+            budget.totalExpenses += newValue - oldValue
+            budget.totalExpensesBox.innerText = formatter.format(budget.totalExpenses);
+            item.previousElementSibling.previousElementSibling.firstElementChild.innerText = newValue;
+        
+            computePercentage();
+            computeTotals();
+            updatePercent();
+            updatePerPerson();
+        }
+    });
+})
+
+document.querySelectorAll('.quantity__info-buttons').forEach(item => {
+    item.addEventListener('click', () =>{
+        console.log(item.value)
+        if (item.value === '+'){
+            oldCount = parseFloat(item.previousElementSibling.value);
+            newCount = parseFloat(oldCount) + 0.5
+            originalPrice = item.previousElementSibling.getAttribute('id');
+
+            newValue = newCount * originalPrice
+            oldValue = oldCount * originalPrice
+            budget.totalExpenses += newValue - oldValue
+
+            item.previousElementSibling.innerText = newCount
+            item.previousElementSibling.value = newCount
+
+            budget.totalExpensesBox.innerText = formatter.format(budget.totalExpenses);
+            item.previousElementSibling.previousElementSibling.previousElementSibling.firstElementChild.innerText = newValue;
+        } else if (item.value === '-'){
+            oldCount = parseFloat(item.nextElementSibling.value)
+            if (oldCount > 0){
+                newCount = parseFloat(oldCount) - 0.5
+                originalPrice = item.nextElementSibling.getAttribute('id');
+
+                newValue = newCount * originalPrice
+                oldValue = oldCount * originalPrice
+                budget.totalExpenses += newValue - oldValue
+
+                item.nextElementSibling.innerText = newCount
+                item.nextElementSibling.value = newCount 
+
+                budget.totalExpensesBox.innerText = formatter.format(budget.totalExpenses);
+                item.previousElementSibling.firstElementChild.innerText = newValue;
+            }
+        }
+        computePercentage();
+        computeTotals();
+        updatePercent();
+        updatePerPerson();
+    })
+});
 
 function addList() {   
     createItem();
     computePercentage();
     computeTotals();
     updatePercent();
+    updatePerPerson();
 }
 
 function createItem(){
@@ -176,3 +258,10 @@ function updatePercent(){
     }
 }
 
+function updatePerPerson(){
+    budget.totalPeople = document.querySelector('.add__number__of__people').value;
+    budget.totalNumberOfPeopleBox.innerText = `${budget.totalPeople} pax`
+
+    budget.netPerPerson = parseFloat(budget.totalExpenses/budget.totalPeople);
+    budget.totalExpensePerPersonBox.innerText = formatter.format(budget.netPerPerson);
+}
